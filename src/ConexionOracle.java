a4712e29e15a1ffd01adae500d6b8263faa2e3ff
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-05 17:02:44 -0500

    commit - FuncionLineal . /

--- src/ConexionOracle.java
+++ src/ConexionOracle.java
@@ -6,7 +6,7 @@ public class ConexionOracle {
 
     private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
     private static final String USER = "SYSTEM";
-    private static final String PASSWORD = "Tapiero123";    
+    private static final String PASSWORD = "Tapiero123";
 
     public static Connection getConexion() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
--- src/FuncionLineal.java
+++ src/FuncionLineal.java
@@ -1,2 +1,24 @@
 public class FuncionLineal {
-}
+
+    private double m;
+    private double b;
+
+    public FuncionLineal(double m, double b) {
+        this.m = m;
+        this.b = b;
+    }
+
+    // retorna vector bidimensional {x, y}
+    public double[] calcular(double x) {
+        double y = m * x + b;
+        return new double[]{x, y};
+    }
+
+    public double getM() {
+        return m;
+    }
+
+    public double getB() {
+        return b;
+    }
+}   
